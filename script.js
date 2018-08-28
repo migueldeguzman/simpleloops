86271318084ff8c8437dd910a008805cc920b9ce
migueldeguzman <40692613+migueldeguzman@users.noreply.github.com>
2018-08-28 13:21:48 +0800

    adding html description

--- script.js
+++ script.js
@@ -1,75 +1,32 @@
 //for loop
-var todos = [
+var toDo = [
 	"clean room",
 	"brush teeth",
 	"exercise",
 	"study javascript",
 	"eat healthy"
-];
+];	
 
-for (var i=0; i< todos.length; i++) {
-	console.log(todos[i] = todos[i] + "!");
+console.log("toDo", toDo);
+
+var workDone = [];
+
+for (var i=0; i < toDo.length; i++) {
+	console.log(workDone[i] = toDo[i] + " - done!");
 }
 
-console.log(todos);
+console.log("workDone", workDone);
 
-// var todosLength = todos.length;
-// for (var i=0; i< todosLength; i++) {
-// 	console.log(todos.pop());
-// }
+var toDoList = []; 
 
-logTodos = function(i) {
-	console.log("forEach", i);
-}
+toDo.forEach(function(i){
+	console.log(toDoList[i] = toDo[i] + " - needs rework!");	
+})
 
-todos.forEach(logTodos);
+console.log("toDoList", toDoList);
 
-
-
-
-
-console.log(todos);
-
-// //while loop count up!
-// var counterOne = 0;
-// while (counterOne < 10) {
-// 	console.log("while", counterOne);
-// 	counterOne++;
-// }
-
-// //while loop count down!
-// var counterOne = 10;
-// while (counterOne > 0) {
-// 	console.log("while", counterOne);
-// 	counterOne--;
-// }
-
-
-// //infiniteloop!
-// // var counterOne = 10;
-// // while (counterOne > 0) {
-// // 	console.log(counterOne);
-// // 	counterOne++;
-// // }
-
-// //do while
-// var counterTwo = 10
-// do {
-// 	console.log("do while", counterTwo);
-// 	counterTwo--;
-// } while (counterTwo > 0);
-
-// //do while
-// var counterTwo = 10
-// do {
-// 	console.log("do while", counterTwo);
-// 	counterTwo--;
-// } while (counterTwo > 10);
-
-// //while loop count down!
-// var counterOne = 10;
-// while (counterOne > 10) {
-// 	console.log("while", counterOne);
-// 	counterOne--;
-// }
-
+/*var counterOne = 10;
+while (counterOne > 0) {
+	console.log(counterOne);
+	counterOne--;
+}*/
